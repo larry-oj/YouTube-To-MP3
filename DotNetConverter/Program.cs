c3f68bcf2ea7603877994dbd250fc5766c252e34
Larry <66905431+larry-oj@users.noreply.github.com>
2022-07-03 14:46:41 +0300

    register queue

--- DotNetConverter/Program.cs
+++ DotNetConverter/Program.cs
@@ -1,9 +1,12 @@
+using DotNetConverter.Services.Queues;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddLogging();
 builder.Services.AddHttpClient();
+builder.Services.AddSingleton<IVideoQueue, VideoQueue>();
 
 var app = builder.Build();
 
