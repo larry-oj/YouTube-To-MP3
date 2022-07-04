3fedf3cc93b2d3a2ad2895ca470a82ecad230a6d
Larry <66905431+larry-oj@users.noreply.github.com>
2022-07-04 12:42:22 +0300

    implement controller

---
+++ DotNetConverter/Controllers/ConverterController.cs
@@ -0,0 +1,48 @@
+﻿using DotNetConverter.Models;
+using DotNetConverter.Services;
+using DotNetConverter.Services.Queues;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetConverter.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ConversionController : ControllerBase
+{
+    private readonly IVideoQueue _queue;
+    private readonly ILogger<ConversionController> _logger;
+
+    public ConversionController(IVideoQueue queue, 
+        ILogger<ConversionController> logger)
+    {
+        _queue = queue;
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> QueueVideo([FromBody] QueueVideoRequest request, CancellationToken token)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        try
+        {
+            await _queue.QueueWorkItemAsync(token, request.Id, request.Url, request.CallbackUrl);
+        }
+        catch (Exception ex)
+        {
+            if (ex is ArgumentNullException or ArgumentException)
+            {
+                _logger.LogError("Returning error response");
+                return BadRequest(ex.Message);
+            }
+            else
+            {
+                _logger.LogError(ex, "Returning server error response");
+                return StatusCode(500, "Sorry, something went wrong");
+            }
+        }
+
+        return Ok();
+    }
+}
---
+++ DotNetConverter/Models/QueueVideoRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace DotNetConverter.Models;
+
+public class QueueVideoRequest
+{
+    [Required]
+    [JsonPropertyName("id")] 
+    public string Id { get; set; }
+    
+    [Required]
+    [JsonPropertyName("url")] 
+    public string Url { get; set; }
+    
+    [Required]
+    [JsonPropertyName("callback_url")] 
+    public string CallbackUrl { get; set; }
+}
