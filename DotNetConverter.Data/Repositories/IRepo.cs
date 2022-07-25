9a675643df5ab1e51424e28e2e9ed6b08518fda7
Larry <66905431+larry-oj@users.noreply.github.com>
2022-07-25 13:15:43 +0300

    implement repository pattern

---
+++ DotNetConverter.Data/Repositories/IRepo.cs
@@ -0,0 +1,13 @@
+﻿using DotNetConverter.Data.Models;
+
+namespace DotNetConverter.Data.Repositories;
+
+public interface IRepo<T> where T : IStringIdEntity
+{
+    IEnumerable<T> GetAll();
+    T? Get(string id);
+    void Insert(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+    Task SaveAsync();
+}
---
+++ DotNetConverter.Data/Repositories/Repo.cs
@@ -0,0 +1,49 @@
+﻿using DotNetConverter.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetConverter.Data.Repositories;
+
+public class Repo<T> : IRepo<T> where T : class, IStringIdEntity
+{
+    private readonly ConverterDbContext _context;
+    private readonly DbSet<T> _entities;
+
+    public Repo(ConverterDbContext context)
+    {
+        _context = context;
+        _entities = _context.Set<T>();
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+        return _entities.AsEnumerable();
+    }
+
+    public T? Get(string id)
+    {
+        return _entities.FirstOrDefault(e => e.Id == id);
+    }
+
+    public void Insert(T entity)
+    {
+        if (entity == null) throw new ArgumentNullException(nameof(entity));
+        _entities.Add(entity);
+    }
+
+    public void Update(T entity)
+    {
+        if (entity == null) throw new ArgumentNullException(nameof(entity));
+        _entities.Update(entity);
+    }
+
+    public void Delete(T entity)
+    {
+        if (entity == null) throw new ArgumentNullException(nameof(entity));
+        _entities.Remove(entity);
+    }
+
+    public async Task SaveAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
