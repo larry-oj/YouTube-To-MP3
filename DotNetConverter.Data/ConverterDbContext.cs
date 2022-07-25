a70252853454076580efa82d34a764f5b0076226
Larry <66905431+larry-oj@users.noreply.github.com>
2022-07-25 12:38:18 +0300

    Add queued items entity

--- DotNetConverter.Data/ConverterDbContext.cs
+++ DotNetConverter.Data/ConverterDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DotNetConverter.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DotNetConverter.Data;
 
 public class ConverterDbContext : DbContext
 {
-    // ...
+    public DbSet<QueuedItem> QueuedItems { get; set; }
     
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
---
+++ DotNetConverter.Data/Models/QueuedItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetConverter.Data.Models;
+
+public class QueuedItem
+{
+    [Key] public string Id { get; set; }
+    public bool IsFinished { get; set; } = false;
+    public DateTime? TimeFinished { get; set; }
+    public string Data { get; set; }
+
+    public QueuedItem(string id, string data)
+    {
+        Id = id;
+        Data = data;
+    }
+}
