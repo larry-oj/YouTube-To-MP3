4faf2adbe1e2c1189d78b19fce8d8a175e307fea
Larry <66905431+larry-oj@users.noreply.github.com>
2022-07-25 22:48:10 +0300

    make callback url unnecessairy

--- DotNetConverter/Models/QueueVideoRequest.cs
+++ DotNetConverter/Models/QueueVideoRequest.cs
@@ -7,5 +7,5 @@ public class QueueVideoRequest
 {
     [Required] [JsonPropertyName("url")] public string Url { get; set; }
     [JsonPropertyName("with_callback")] public bool WithCallback { get; set; }
-    [JsonPropertyName("callback_url")] public string CallbackUrl { get; set; }
+    [JsonPropertyName("callback_url")] public string? CallbackUrl { get; set; }
 }
