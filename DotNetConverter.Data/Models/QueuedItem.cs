c129189ea1f8355637d0e23cf1127d0cd8dbb18d
Larry <66905431+larry-oj@users.noreply.github.com>
2022-07-26 11:16:25 +0300

    add name field to database

--- DotNetConverter.Data/Models/QueuedItem.cs
+++ DotNetConverter.Data/Models/QueuedItem.cs
@@ -5,11 +5,13 @@ namespace DotNetConverter.Data.Models;
 public class QueuedItem : IStringIdEntity
 {
     [Key] public string Id { get; set; }
+    [Required] public string Name { get; set; }
     public bool IsFinished { get; set; } = false;
     public DateTime? TimeFinished { get; set; }
 
-    public QueuedItem(string id)
+    public QueuedItem(string id, string name)
     {
         Id = id;
+        Name = name;
     }
 }
